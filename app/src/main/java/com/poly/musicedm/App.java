f26e0a19ada39e1c37d1c3edda5d30ffdb5c197c
Vuong Phan <vuongphan04467@gmail.com>
2017-10-18 21:02:49 +0700

    create project

---
+++ app/src/main/java/com/poly/musicedm/App.java
@@ -0,0 +1,18 @@
+package com.poly.musicedm;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    private static App app;
+
+    public static App getInstance() {
+        return app;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+    }
+}
